1e429b7939dd5e2295a02df035fb3ca6dc637125
Nopallse <2211522020_naufal@student.unand.ac.id>
2026-01-19 15:47:24 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  
+  compress: true,
+  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ryupowertools.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'nest-api.ryupowertools.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
